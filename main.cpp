b9f2a9c628298b000e1bc11389e40a92cf6bca26
Nathan Coustance <31705343+NathanRaki@users.noreply.github.com>
2021-04-11 12:09:55 +0200

    Updated DCT calculation, added ZigZag Scan

--- main.cpp
+++ main.cpp
@@ -4,82 +4,129 @@
 #include <vector>
 
 # define M_PI 3.14159265358979323846
-typedef std::vector<std::vector<float>> block;
+typedef std::vector<std::vector<int>> block;
+typedef std::vector<std::vector<block>> grid;
+typedef std::pair<int, int> pos;
 
-void get_dct(block& in, block& out)
+// Quantize a DCT block with luminance quantization table
+// in : DCT 8x8 matrix
+// out (output) : Quantized DCT 8x8 matrix
+void quantize(block& in, block& out)
 {
-	float Cu, Cv, sum, intensity;
-	int x, y, u, v;
-
-	for (u = 0; u < 8; u++)
+	// Importing the luminance quantization table
+	float q_table[8][8] = { {16, 11, 10, 16,  24,  40,  51,  61},
+							{12, 12, 14, 19,  26,  58,  60,  55},
+							{14, 13, 16, 24,  40,  57,  69,  56}, 
+							{14, 17, 22, 29,  51,  87,  80,  62}, 
+							{18, 22, 37, 56,  68, 109, 103,  77},
+							{24, 35, 55, 64,  81, 104, 113,  92}, 
+							{49, 64, 78, 87, 103, 121, 120, 101}, 
+							{72, 92, 95, 98, 112, 100, 103,  99} };
+	// Go through the block and quantize every value
+	for (int x = 0; x < 8; x++)
 	{
-		std::vector<float> row;
-		for (v = 0; v < 8; v++)
+		std::vector<int> row;
+		for (int y = 0; y < 8; y++)
 		{
-			sum = 0;
-			if (u == 0) { Cu = 1 / sqrt(2); }
-			else { Cu = 1; }
-			if (v == 0) { Cv = 1 / sqrt(2); }
-			else { Cv = 1; }
-			for (x = 0; x < 8; x++)
-			{
-				for (y = 0; y < 8; y++)
-				{
-					intensity = in[x][y];
-					sum +=
-						intensity *
-						cos(((2.0 * x + 1) * u * M_PI) / 16.0) *
-						cos(((2.0 * y + 1) * v * M_PI) / 16.0);
-				}
-			}
-			row.push_back(0.25 * Cu * Cv * sum);
+			row.push_back(round(in[x][y] / q_table[x][y]));
 		}
 		out.push_back(row);
 	}
 }
 
-void get_dcts(cv::Mat& in, std::vector<block>& out)
+// Convert image to a grid of 8x8 DCT blocks
+// img : Image you want to convert
+// dct (output) : Grid of 8x8 DCT blocks
+void convertToDCT(cv::Mat& img, grid& dct)
 {
-	cv::Mat in_f;
-	in.convertTo(in_f, CV_32F);
-	for (int x = 0; x < in.rows; x += 8)
+	cv::Mat img_f;
+	img.convertTo(img_f, CV_32F); // Convert image to float values instead of uchar
+	block b(8, std::vector<int>(8)); // 8x8 vector, filled with zeros
+
+	// Iterating through blocks (step=8)
+	for (int i = 0; i < img.rows; i+=8)
 	{
-		for (int y = 0; y < in.cols; y += 8)
+		std::vector<block> grid_row; // Row of blocks, to fill the grid
+		for (int j = 0; j < img.cols; j+=8)
 		{
-			block pixels, dct;
-			for (int x_ = 0; x_ < 8; x_++)
+			// Iterating through each block value
+			for (int u = 0; u < 8; u++)
 			{
-				std::vector<float> row;
-				for (int y_ = 0; y_ < 8; y_++)
+				for (int v = 0; v < 8; v++)
 				{
-					row.push_back(in_f.at<float>(x + x_, y + y_));
+					float Cu, Cv, sum = 0;
+					if (u == 0) { Cu = 1 / sqrt(2); } else { Cu = 1; } // cf. DCT formula
+					if (v == 0) { Cv = 1 / sqrt(2); } else { Cv = 1; } // cf. DCT formula
+					// Iterating through each pixel
+					for (int x = 0; x < 8; x++)
+					{
+						for (int y = 0; y < 8; y++)
+						{
+							sum += img_f.at<float>(i+x, j+y)
+									* cos(((2.0 * x + 1) * u * M_PI) / 16.0)
+									* cos(((2.0 * y + 1) * v * M_PI) / 16.0);
+						}
+					}
+					// Once we've seen every pixel, we can update the dct coefficient value and go to the next one
+					b[u][v] = round(0.25 * Cu * Cv * sum);
 				}
-				pixels.push_back(row);
 			}
-			get_dct(pixels, dct);
-			out.push_back(dct);
+			grid_row.push_back(b); // Adding the block to the row
 		}
+		dct.push_back(grid_row); // Adding the row to the grid
 	}
 }
 
-void print_dct(std::vector<block>& dcts, int id)
+// C++ ZigZag Scan Algorithm
+// Based on https://gist.github.com/gokercebeci/10556381
+// Thanks to gokercebeci
+bool buble(std::vector<int> i, std::vector<int> j)
 {
-	block dct = dcts[id];
-	for (std::vector<float> row : dct)
-	{
-		for (float v : row)
-		{
-			printf("%8.1f ", v);
-		}
-		std::cout << std::endl;
+	return  (i[0] == j[0]) ? (i[1] < j[1]) : (i[0] < j[0]);
+}
+std::vector<pos> zigzagscan(int n, int m)
+{
+	std::vector<pos> res;
+	std::vector< std::vector<int> > p(n * m, std::vector<int>(3));
+	for (int i = 0; i < n * m; i++) {
+		p[i][0] = i % n + i / n;
+		p[i][1] = (((i % n - i / n) % 2) == 0 ? i % n : i / n);
+		p[i][2] = i;
 	}
+	std::sort(p.begin(), p.end(), buble);
+
+	for (int i = 0; i < n * m; i++)
+		res.push_back(std::make_pair(int(p[i][2]/m), p[i][2]%n));
+	return res;
 }
 
 int main()
 {
+	// Convert image to DCT grid
 	cv::Mat img = cv::imread("panther.jpg", cv::IMREAD_GRAYSCALE);
-	std::vector<block> dcts;
-	get_dcts(img, dcts);
-	print_dct(dcts, 5500); // 5500th block
+	grid g;
+	convertToDCT(img, g);
+
+	// Display block content
+	block b = g[30][99]; // 31st row, 100th column
+	for (auto x : b)
+	{
+		for (auto y : x)
+		{
+			printf("%5i ", y);
+		}
+		std::cout << std::endl;
+	}
+
+	// Display block content in zigzag scan order
+	std::vector<pos> order = zigzagscan(8, 8);
+	for (auto x : order)
+	{
+		printf("%5i ", b[x.first][x.second]);
+	}
+
+	int DC_pos = 0; // DC coefficient pos in zigzag scan
+	int lastMF_pos = 48; // Last Middle Frequency pos in zigzag scan
+	
 	return 0;
 }
